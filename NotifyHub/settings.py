3a4c4cd89cf8a125ee81942f2b786536dd8d7b04
Bipul Dawadi <bipuldawadi14@gmail.com>
2025-07-24 13:21:29 +0545

    Sqlite Config  for Production

--- NotifyHub/settings.py
+++ NotifyHub/settings.py
@@ -124,14 +124,12 @@ if DEBUG:
         }
     }
 else:
-        DATABASES = {
-        "default": dj_database_url.config(
-            default=config("DATABASE_URL"),
-            conn_max_age=600,
-            ssl_require=False
-        )
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',  
+            'NAME': BASE_DIR / 'db.sqlite3',  
+        }
     }
-
 
 
 # Password validation
